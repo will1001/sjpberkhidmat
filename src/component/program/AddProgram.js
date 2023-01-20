ff373469a6e288cd7ebeea4b01c0fa353bb7abc1
wiligetsurvey <wili@getsurvey.id>
2023-01-20 10:04:59 +0800

    perbaiki form program

--- src/component/program/AddProgram.js
+++ src/component/program/AddProgram.js
@@ -58,7 +58,7 @@ const AddProgram = () => {
     image: "",
     publication: false,
   });
-  const kabupaten = useFetch("get", "user/kabupaten");
+  const kabupaten = useFetch("get", "user/kabupaten?filter=lombok");
   const artikel = useFetch("get", "user/articles?page=1");
 
   const categoryProgram = [
@@ -125,7 +125,15 @@ const AddProgram = () => {
 
   const [berhasil, setBerhasil] = useState(false);
   const handleBerhasil = () => {
-    setFormProgram({ ...formProgram, title: "", category: "", description: "", image: "", publication: false, wilayah: "" });
+    setFormProgram({
+      ...formProgram,
+      title: "",
+      category: "",
+      description: "",
+      image: "",
+      publication: false,
+      wilayah: "",
+    });
     setPopUp(false);
     setBerhasil(false);
   };
@@ -134,26 +142,60 @@ const AddProgram = () => {
 
   return (
     <>
-      <div className={`bg-slate-400 bg-opacity-50 absolute w-screen top-0 h-[1100px] ${popUp === true ? "visible" : "hidden"}`}>
+      <div
+        className={`bg-slate-400 bg-opacity-50 absolute w-screen top-0 h-[1100px] ${
+          popUp === true ? "visible" : "hidden"
+        }`}
+      >
         <div className="h-[410px] w-[620px] ml-[416px] mt-[120px] bg-white absolute">
-          <div onClick={() => setPopUp(false)} className="h-[24px] w-[24] pr-2  absolute top-0 right-0 text-[24px] font-semibold text-[#9CA3AF] cursor-pointer">
+          <div
+            onClick={() => setPopUp(false)}
+            className="h-[24px] w-[24] pr-2  absolute top-0 right-0 text-[24px] font-semibold text-[#9CA3AF] cursor-pointer"
+          >
             X
           </div>
-          <div className="flex justify-center mt-[30px]">{berhasil === false ? <img src={publikasiProgram.src} alt="publikasi_program.png" /> : <img src={berhasilImg.src} alt="berhasil.png" />}</div>
-          <p className="text-[32px] text-[#374151] font-bold flex justify-center pt-[32px] pb-[16px]">{berhasil === false ? "Publikasikan Program?" : "Publikasi Berhasil"}</p>
-          <p className="text-[#374151] flex justify-center pb-[32px]">{berhasil === false ? "anda akan menambahkan data program" : "Program telah ditambakan"}</p>
+          <div className="flex justify-center mt-[30px]">
+            {berhasil === false ? (
+              <img src={publikasiProgram.src} alt="publikasi_program.png" />
+            ) : (
+              <img src={berhasilImg.src} alt="berhasil.png" />
+            )}
+          </div>
+          <p className="text-[32px] text-[#374151] font-bold flex justify-center pt-[32px] pb-[16px]">
+            {berhasil === false
+              ? "Publikasikan Program?"
+              : "Publikasi Berhasil"}
+          </p>
+          <p className="text-[#374151] flex justify-center pb-[32px]">
+            {berhasil === false
+              ? "anda akan menambahkan data program"
+              : "Program telah ditambakan"}
+          </p>
           <div className="flex justify-center items-center gap-8">
             {berhasil === false ? (
               <>
-                <div onClick={() => setPopUp(false)} className="cursor-pointer w-[184px] h-[49px] border border-[#9CA3AF] rounded-sm flex items-center justify-center">
-                  <p className="text-[18px] text-[#374151] font-semibold">Batal</p>
+                <div
+                  onClick={() => setPopUp(false)}
+                  className="cursor-pointer w-[184px] h-[49px] border border-[#9CA3AF] rounded-sm flex items-center justify-center"
+                >
+                  <p className="text-[18px] text-[#374151] font-semibold">
+                    Batal
+                  </p>
                 </div>
-                <div onClick={postArtikel} className="cursor-pointer w-[184px] h-[49px] bg-[#FF5001] rounded-sm flex items-center justify-center">
-                  <p className="text-[18px] text-[#fff] font-semibold">Publikasikan</p>
+                <div
+                  onClick={postArtikel}
+                  className="cursor-pointer w-[184px] h-[49px] bg-[#FF5001] rounded-sm flex items-center justify-center"
+                >
+                  <p className="text-[18px] text-[#fff] font-semibold">
+                    Publikasikan
+                  </p>
                 </div>
               </>
             ) : (
-              <div onClick={handleBerhasil} className="cursor-pointer w-[184px] h-[49px] bg-[#FF5001] rounded-sm flex items-center justify-center">
+              <div
+                onClick={handleBerhasil}
+                className="cursor-pointer w-[184px] h-[49px] bg-[#FF5001] rounded-sm flex items-center justify-center"
+              >
                 <p className="text-[18px] text-[#fff] font-semibold">OK</p>
               </div>
             )}
@@ -162,7 +204,9 @@ const AddProgram = () => {
       </div>
       <div className="flex pl-[42px] mt-[32px] gap-4 border-b-2">
         <Logo />
-        <p className="text-[26px] font-semibold font-serif text-[#374151] pr-[450px]">Publikasi Program</p>
+        <p className="text-[26px] font-semibold font-serif text-[#374151] pr-[450px]">
+          Publikasi Program
+        </p>
         {/* button publikasian */}
         <div onClick={() => setPopUp(!popUp)}>
           <NewButton title={"Publikasikan"} style={publikasiStyle} />
@@ -181,7 +225,9 @@ const AddProgram = () => {
               Judul Program
             </label>
             <input
-              onChange={(e) => setFormProgram({ ...formProgram, title: e.target.value })}
+              onChange={(e) =>
+                setFormProgram({ ...formProgram, title: e.target.value })
+              }
               className="border border-[#D1D5DB] h-[48px] outline-0 rounded-md p-[12px] text-[#374151] font-medium"
               value={formProgram.title}
               type={"text"}
@@ -189,31 +235,61 @@ const AddProgram = () => {
             />
 
             {/* description  */}
-            <label id="title" className="text-[#6B7280] text-[16px] font-serif pt-[60px]">
+            <label
+              id="title"
+              className="text-[#6B7280] text-[16px] font-serif pt-[60px]"
+            >
               detail Program
             </label>
             <div className="flex h-[72px] bg-[#6B7280] border rounde-sm items-center px-[16px] text-[#374151] font-serif gap-6">
               <div className="bg-white h-[48px] rounded-md border w-[183px] flex">
                 <label id="heading"></label>
-                <select style={{ WebkitAppearance: "none", backgroundImage: `url(${selectIcon.src})` }} id="heading" className="outline-0 cursor-pointer font-medium mx-[14px] w-full bg-right bg-no-repeat">
+                <select
+                  style={{
+                    WebkitAppearance: "none",
+                    backgroundImage: `url(${selectIcon.src})`,
+                  }}
+                  id="heading"
+                  className="outline-0 cursor-pointer font-medium mx-[14px] w-full bg-right bg-no-repeat"
+                >
                   <option value="1">Heading 1</option>
                   <option value="2">Heading 2</option>
                 </select>
               </div>
               <div className="flex gap-2">
-                <div className="flex bg-white h-[48px] w-[48px] font-semibold items-center justify-center border rounded-md cursor-pointer">B</div>
-                <div className="flex bg-white h-[48px] w-[48px] italic items-center justify-center border rounded-md cursor-pointer">I</div>
-                <div className="flex bg-white h-[48px] w-[48px] underline items-center justify-center border rounded-md cursor-pointer">U</div>
+                <div className="flex bg-white h-[48px] w-[48px] font-semibold items-center justify-center border rounded-md cursor-pointer">
+                  B
+                </div>
+                <div className="flex bg-white h-[48px] w-[48px] italic items-center justify-center border rounded-md cursor-pointer">
+                  I
+                </div>
+                <div className="flex bg-white h-[48px] w-[48px] underline items-center justify-center border rounded-md cursor-pointer">
+                  U
+                </div>
               </div>
               <div className="flex gap-2">
-                <div style={{ backgroundImage: `url(${center.src})` }} className="w-[48px] h-[48px] bg-white bg-no-repeat bg-center border rounded-md cursor-pointer" />
-                <div style={{ backgroundImage: `url(${left.src})` }} className="w-[48px] h-[48px] bg-white bg-no-repeat bg-center border rounded-md cursor-pointer" />
-                <div style={{ backgroundImage: `url(${justify.src})` }} className="w-[48px] h-[48px] bg-white bg-no-repeat bg-center border rounded-md cursor-pointer" />
-                <div style={{ backgroundImage: `url(${right.src})` }} className="w-[48px] h-[48px] bg-white bg-no-repeat bg-center border rounded-md cursor-pointer" />
+                <div
+                  style={{ backgroundImage: `url(${center.src})` }}
+                  className="w-[48px] h-[48px] bg-white bg-no-repeat bg-center border rounded-md cursor-pointer"
+                />
+                <div
+                  style={{ backgroundImage: `url(${left.src})` }}
+                  className="w-[48px] h-[48px] bg-white bg-no-repeat bg-center border rounded-md cursor-pointer"
+                />
+                <div
+                  style={{ backgroundImage: `url(${justify.src})` }}
+                  className="w-[48px] h-[48px] bg-white bg-no-repeat bg-center border rounded-md cursor-pointer"
+                />
+                <div
+                  style={{ backgroundImage: `url(${right.src})` }}
+                  className="w-[48px] h-[48px] bg-white bg-no-repeat bg-center border rounded-md cursor-pointer"
+                />
               </div>
             </div>
             <textarea
-              onChange={(e) => setFormProgram({ ...formProgram, description: e.target.value })}
+              onChange={(e) =>
+                setFormProgram({ ...formProgram, description: e.target.value })
+              }
               ref={refTextArea}
               id="description_program"
               name="description_program"
@@ -223,31 +299,61 @@ const AddProgram = () => {
           </div>
         </div>
         <div className="basis-4/12  pt-[34px] pl-[50px] pr-[41px] border-l-2">
-          <p className="font-bold text-[#374151] text-[18px] mb-[16px]">Publikasi di Website</p>
+          <p className="font-bold text-[#374151] text-[18px] mb-[16px]">
+            Publikasi di Website
+          </p>
           <div className="flex items-center gap-4">
             <div
               onClick={() => {
                 setSwitchButton(!switchButton);
                 setFormProgram({ ...formProgram, publication: switchButton });
               }}
-              className={`w-[56px] h-[30px] cursor-pointer flex items-center px-[2px] rounded-full ${formProgram.publication === true ? ` bg-[#FF5001]  justify-end` : "bg-[#6B7280]"}`}
+              className={`w-[56px] h-[30px] cursor-pointer flex items-center px-[2px] rounded-full ${
+                formProgram.publication === true
+                  ? ` bg-[#FF5001]  justify-end`
+                  : "bg-[#6B7280]"
+              }`}
             >
               <div className={`bg-white w-[26px] h-[26px] rounded-full`}></div>
             </div>
             <p className="font-medium text-[#374151]">Publikasikan</p>
           </div>
-          <p className="text-[18px] text-[#374151] font-bold pt-[38px] pb-[24px]">Wilayah Tujuan Program</p>
+          <p className="text-[18px] text-[#374151] font-bold pt-[38px] pb-[24px]">
+            Wilayah Tujuan Program
+          </p>
           <div className="flex flex-col gap-2">
-            <label id="kota" value="kota" className="text-[12px] text-[#374151]">
+            <label
+              id="kota"
+              value="kota"
+              className="text-[12px] text-[#374151]"
+            >
               Kabupaten / Kota
             </label>
-            <input
+            {/* <input
               onChange={(e) => setFormProgram({ ...formProgram, id_kabupaten: e.target.value })}
               className="h-[40px] outline-0 border border-[#FF5001] rounded-md p-2 text-[#374151] text-[14px]"
               value={formProgram?.id_kabupaten}
               type={"text"}
               id="kota"
-            />
+            /> */}
+            <select
+              onChange={(e) =>
+                setFormProgram({ ...formProgram, id_kabupaten: e.target.value })
+              }
+              id="kabupaten"
+              className="h-[40px] w-[363px] border text-[#374151]"
+            >
+              <option value="" disabled selected>
+                Pilih Kabupaten
+              </option>
+              {kabupaten.data?.map((res, i) => {
+                return (
+                  <option key={i} value={res._id}>
+                    {res.name}
+                  </option>
+                );
+              })}
+            </select>
             <div className="flex items-center gap-2">
               {kabupaten?.data
                 ?.filter((item, i) => {
@@ -259,7 +365,12 @@ const AddProgram = () => {
                 .map((res) => {
                   return (
                     <div
-                      onClick={() => setFormProgram({ ...formProgram, wilayah: res?.name.toLowerCase() })}
+                      onClick={() =>
+                        setFormProgram({
+                          ...formProgram,
+                          wilayah: res?.name.toLowerCase(),
+                        })
+                      }
                       value={res.name}
                       key={res._id}
                       className="flex font-sans items-center justify-center rounded-full h-[38px] px-[18px] bg-[#374151] text-[12px] text-white"
@@ -269,7 +380,9 @@ const AddProgram = () => {
                   );
                 })}
             </div>
-            <p className="text-[18px] text-[#374151] font-bold pt-[30px]">category Program</p>
+            <p className="text-[18px] text-[#374151] font-bold pt-[30px]">
+              category Program
+            </p>
             {categoryProgram.map((res) => {
               return (
                 <div
@@ -281,12 +394,28 @@ const AddProgram = () => {
                   value={selectCategory}
                   className="flex items-center gap-4"
                 >
-                  <div className={`h-[30px] w-[30px] rounded-full cursor-pointer ${res.name === selectCategory ? "bg-[#FF5001]" : "border-2 border-[#D1D5DB]"}  `} />
-                  <p className={`text-[16px] font-medium ${res.name === selectCategory ? "text-[#FF5001]" : "text-[#374151]"}`}>{res.name}</p>
+                  <div
+                    className={`h-[30px] w-[30px] rounded-full cursor-pointer ${
+                      res.name === selectCategory
+                        ? "bg-[#FF5001]"
+                        : "border-2 border-[#D1D5DB]"
+                    }  `}
+                  />
+                  <p
+                    className={`text-[16px] font-medium ${
+                      res.name === selectCategory
+                        ? "text-[#FF5001]"
+                        : "text-[#374151]"
+                    }`}
+                  >
+                    {res.name}
+                  </p>
                 </div>
               );
             })}
-            <p className="text-[18px] text-[#374151] font-bold pt-[30px]">Media File (Foto / Video)</p>
+            <p className="text-[18px] text-[#374151] font-bold pt-[30px]">
+              Media File (Foto / Video)
+            </p>
 
             <ImageUploading
               multiple
@@ -300,14 +429,24 @@ const AddProgram = () => {
             >
               {({ onImageUpload, isDragging, dragProps }) => (
                 // write your building UI
-                <div className="h-[112px] border border-[#D1D5DB]" style={isDragging ? { background: "#FF5001" } : undefined}>
+                <div
+                  className="h-[112px] border border-[#D1D5DB]"
+                  style={isDragging ? { background: "#FF5001" } : undefined}
+                >
                   <div className="flex justify-center items-center">
-                    <button className="flex flex-col items-center pt-4" onClick={onImageUpload} {...dragProps}>
+                    <button
+                      className="flex flex-col items-center pt-4"
+                      onClick={onImageUpload}
+                      {...dragProps}
+                    >
                       <img src={uploadFile.src} alt="upload here" />
                       <p className="text-[12px] text-[#000000] font-semibold">
-                        <span className="text-[#FF5001]">Upload a file </span>of drag and drop{" "}
+                        <span className="text-[#FF5001]">Upload a file </span>of
+                        drag and drop{" "}
                       </p>
-                      <p className="text-[12px] font-normal">PNG, JPG, MP4 upto 32MB</p>
+                      <p className="text-[12px] font-normal">
+                        PNG, JPG, MP4 upto 32MB
+                      </p>
                     </button>
                   </div>
                 </div>
