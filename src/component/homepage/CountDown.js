5069b4caea7ee2676d163feb8a0091aff3a210dd
wiligetsurvey <wili@getsurvey.id>
2023-01-24 18:58:57 +0800

    countdown home page

--- src/component/homepage/CountDown.js
+++ src/component/homepage/CountDown.js
@@ -1,24 +1,41 @@
 import React from "react";
+import Countdown from "react-countdown";
 
 const CountDown = () => {
   return (
     <div className="flex gap-4">
-      <div className="flex flex-col pb-2 justify-center h-[92px] w-[92px] bg-white rounded-lg items-center">
-        <p className="text-[49px] font-bold text-[#FF5001]">7</p>
-        <p className="text-[#FF5001] text-[18px]">Hari</p>
-      </div>
-      <div className="flex flex-col pb-2 justify-center h-[92px] w-[92px] bg-white rounded-lg items-center">
-        <p className="text-[49px] font-bold text-[#FF5001]">14</p>
-        <p className="text-[#FF5001] text-[18px]">Jam</p>
-      </div>
-      <div className="flex flex-col pb-2 justify-center h-[92px] w-[92px] bg-white rounded-lg items-center">
-        <p className="text-[49px] font-bold text-[#FF5001]">15</p>
-        <p className="text-[#FF5001] text-[18px]">Menit</p>
-      </div>
-      <div className="flex flex-col pb-2 justify-center h-[92px] w-[92px] bg-white rounded-lg items-center">
-        <p className="text-[49px] font-bold text-[#FF5001]">52</p>
-        <p className="text-[#FF5001] text-[18px]">Detik</p>
-      </div>
+      <Countdown
+        date={Date.now() + (1707840000000 - Date.now())}
+        renderer={(props) => (
+          <>
+            <div className="flex flex-col pb-2 justify-center h-[92px] w-[92px] bg-white rounded-lg items-center">
+              <p className="text-[49px] font-bold text-[#FF5001]">
+                {props.days}
+              </p>
+              <p className="text-[#FF5001] text-[18px]">Hari</p>
+            </div>
+            <div className="flex flex-col pb-2 justify-center h-[92px] w-[92px] bg-white rounded-lg items-center">
+              <p className="text-[49px] font-bold text-[#FF5001]">
+                {props.hours}
+              </p>
+              <p className="text-[#FF5001] text-[18px]">Jam</p>
+            </div>
+            <div className="flex flex-col pb-2 justify-center h-[92px] w-[92px] bg-white rounded-lg items-center">
+              <p className="text-[49px] font-bold text-[#FF5001]">
+                {props.minutes}
+              </p>
+              <p className="text-[#FF5001] text-[18px]">Menit</p>
+            </div>
+            <div className="flex flex-col pb-2 justify-center h-[92px] w-[92px] bg-white rounded-lg items-center">
+              <p className="text-[49px] font-bold text-[#FF5001]">
+                {props.seconds}
+              </p>
+              <p className="text-[#FF5001] text-[18px]">Detik</p>
+            </div>
+          </>
+        )}
+      />
+      ,
     </div>
   );
 };
