8d89870211411ecc3a4a5fdc912f879d6b0c5221
wiligetsurvey <wili@getsurvey.id>
2023-01-25 18:26:18 +0800

    filter sort dan search dashboard relawan

--- src/component/SearchInput.js
+++ src/component/SearchInput.js
@@ -1,12 +1,12 @@
 import React from "react";
 import cariProgramIcon from "../../src/utility/icon/searchIcon.png";
 
-function SearchInput({placeholder}) {
+function SearchInput({ placeholder, onChange }) {
   return (
     <div className="border border-gray-500 border-1 rounded-lg p-[3px]">
       {" "}
       <input
-        // onChange={(e) => setSearch(e.target.value)}
+        onChange={onChange}
         style={{ backgroundImage: `url(${cariProgramIcon.src})` }}
         className="w-[317px] bg-no-repeat bg-right h-[40px] rounded-md outline-0 pl-2 pr-8 text-[14px] text-slate-800 font-semibold"
         type={"text"}
