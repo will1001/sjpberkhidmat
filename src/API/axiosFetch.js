c3584606eaf3a08bd11390da112e31bfa90b89f8
wiligetsurvey <wili@getsurvey.id>
2023-02-08 01:14:00 +0800

    perbaiki input partai dan calon

--- src/API/axiosFetch.js
+++ src/API/axiosFetch.js
@@ -3,7 +3,9 @@ import axios from "axios";
 const axiosFetch = (method, url, data = {}) => {
   const base_url = "https://api.sjpberkhidmat.id/";
   const headers = {
-    authorization: "Bearer " + process.env.REACT_APP_API_TOKEN,
+    authorization:
+      "Bearer " +
+      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCIsImlzcyI6IjU5UUk5NVpQZk5vUXVzekhyemRXYlUxeGNKc3lhWVI5In0.eyJpZCI6ImEyOTM4NjBlLThhYmUtNGExMy1hMmI2LWVhNDAxNWVkYjEyNCIsImVtYWlsIjoiYWRtaW5TSlBAc2pwYmVya2hpZG1hdC5pZCIsImlhdCI6MTY3NTc4OTE4NX0.F91lLHDCkDC-QaOoEUNu0tTbsYhsE_c1Gy2kEqL4qNc",
   };
 
   const res = axios({
