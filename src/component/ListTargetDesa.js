29bc4f4d6aa459718682457e753d43e4b5115101
wiligetsurvey <wili@getsurvey.id>
2023-02-07 17:46:12 +0800

    detail dan target desa

--- src/component/ListTargetDesa.js
+++ src/component/ListTargetDesa.js
@@ -1,6 +1,8 @@
+import { useRouter } from "next/router";
 import React from "react";
 
 function ListTargetDesa({ label, id_kab }) {
+  const router = useRouter();
   return (
     <div className="flex items-center justify-between shadow-md p-[20px] w-[1000px]">
       <div>{label}</div>
@@ -8,9 +10,17 @@ function ListTargetDesa({ label, id_kab }) {
         <div className="mr-[100px]">
           5.000 /<span className="text-orange-600 font-bold">25.000</span>
         </div>
-        <a href="" className="underline text-orange-600">
+        <span
+          onClick={() => {
+            router.push({
+              pathname: "/DetailTargetDesa",
+              query: {},
+            });
+          }}
+          className="underline text-orange-600"
+        >
           lihat detail
-        </a>
+        </span>
       </div>
     </div>
   );
