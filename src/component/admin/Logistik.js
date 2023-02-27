e36d719e82b78b08eec7f4923aa106933766fb36
arimuhamad97 <kedemok98@gmail.com>
2023-02-27 14:32:13 +0800

    logistik broadcast

--- src/component/admin/Logistik.js
+++ src/component/admin/Logistik.js
@@ -1,73 +1,170 @@
 import React, { useState } from "react";
 import { BackIcon, ChatIcon, DeletIcon, DropDownIcon, NextIcon, PrevIcon, ReadIcon, RelawanIcon, SearchIcon } from "../../utility/icon/icon";
 import ChatInput from "../logistik/ChatInput";
+import alert from "../../utility/img/alert_broadcast.png";
+import forum from "../../utility/img/forum.png";
 
 const Logistik = () => {
   const [popup, setPopup] = useState(false);
-  const [chat, setChat] = useState();
+  const [popupPage, setPopupPage] = useState();
+
+  const handlePopUp = (name) => {
+    setPopup(true);
+    name !== popupPage ? setPopupPage(name) : setPopupPage();
+  };
 
   return (
     <>
       {/* popup chat */}
       <div style={{ visibility: popup === false ? "hidden" : "visible" }} className="fixed  top-0 left-0 bg-[#37415152] w-screen h-screen">
-        <div className="absolute inset-y-0 scrollbar scrollbar-thumb-[#9CA3AF] scrollbar-track-[#E5E7EB]  overflow-scroll  right-0 bg-white text-[#374151]">
-          <div onClick={() => setPopup(false)} className="absolute pl-2 text-[21px] text-[#9CA3AF] cursor-pointer font-medium">
-            X
-          </div>
-          <div className="px-[50px] py-[30px]">
-            <p className="text-[21px] font-bold">Logistik Kota Mataram</p>
-            <div className="flex gap-4 mt-2">
-              <p className="w-[140px] text-[#6B7280]">Tgl Pengajuan</p>
-              <p>11/12/23</p>
-            </div>
-            <div className="flex gap-4 mt-2">
-              <p className="w-[140px] text-[#6B7280]">Kebutuhan</p>
-              <p>Flayer</p>
-            </div>
-            <div className="flex gap-4 mt-2">
-              <p className="w-[140px] text-[#6B7280]">Status</p>
-              <p>Belum Input </p>
-            </div>
-            <div className="flex gap-4 mt-2">
-              <p className="w-[140px] text-[#6B7280]">Detail Pengajuan</p>
-              <p className="w-[360px]">Ornare magna ultricies sed dapibus pharetra imperdiet nascetur viverra vitae.</p>
-            </div>
-            <div className="flex gap-4 mt-2">
-              <p className="w-[140px] text-[#6B7280]">Ubah Status</p>
-              <select className="border py-2 px-6 rounded-sm outline-0 cursor-pointer">
-                <option>Proses</option>
-                <option>Menunggu</option>
-                <option>Belum Input</option>
-              </select>
-            </div>
-          </div>
-          <div className="bg-[#F3F4F6] p-[20px] min-h-screen pr-8">
-            {/* chat */}
-            <div className=" overflow-y-auto">
-              <div className="flex justify-center mb-12">
-                <div className="bg-[#E5E7EB] text-[#6B7280] rounded-full px-2 py-1">12 Desember 2022</div>
+        {popupPage === "forum" && (
+          <>
+            <div className="absolute top-[100px] left-[500px] py-[40px] px-[80px] text-[#374151] bg-white">
+              <div
+                onClick={() => {
+                  setPopup(false);
+                  setPopupPage();
+                }}
+                className="absolute right-0 top-0 text-[21px] text-[#9CA3AF] cursor-pointer font-medium pr-2"
+              >
+                X
               </div>
+              <div className="flex flex-col items-center">
+                <div className="flex items-center justify-center">
+                  <img src={forum.src} alt="Forum" />
+                </div>
 
-              <div className="flex">
-                <p className=" font-medium bg-white rounded-t-xl rounded-br-xl shadow-lg my-3 p-2 w-[420px] break-words">
-                  Ornare magna ultricies sed dapibus pharetra imperdiet nascetur viverra vitae.asddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd
-                  <p className="text-end text-[12px] font-normal">10.23</p>
-                </p>
-              </div>
-              <div className="flex justify-end ">
-                <p className="font-medium bg-[#FF5001] text-white  rounded-t-xl rounded-bl-xl shadow-lg my-3 p-2 w-[420px]">
-                  Ornare magna ultricies sed dapibus pharetra imperdiet nascetur viverra vitae.
-                  <p className="text-[12px] font-normal flex gap-1 justify-end items-center">10.23 {<ReadIcon />}</p>
-                </p>
+                <p className="text-[32px] font-bold mt-3">Kirim Ke Forum?</p>
+                <p className="mt-3">anda akan mengirimkan pesan ke semua akun</p>
+                <p>relawan</p>
+                <div className="flex w-full gap-3 justify-between px-3 mt-3">
+                  <div
+                    onClick={() => {
+                      setPopup(true);
+                      setPopupPage("broadcast");
+                    }}
+                    className="border w-full text-center font-medium py-2 rounded-sm cursor-pointer"
+                  >
+                    Batal
+                  </div>
+                  <div className="font-medium text-white bg-[#E44700] rounded-sm cursor-pointer py-2 w-full text-center">Kirim Pesan</div>
+                </div>
               </div>
             </div>
+          </>
+        )}
+        {popupPage === "broadcast" && (
+          <>
+            <div className="absolute top-[100px] left-[400px] p-[20px] text-[#374151] bg-white">
+              <div
+                onClick={() => {
+                  setPopup(false);
+                  setPopupPage();
+                }}
+                className="absolute right-0 top-0 text-[21px] text-[#9CA3AF] cursor-pointer font-medium pr-2"
+              >
+                X
+              </div>
+              <p className="text-[32px] font-bold mb-[42px]">Jumlah Penduduk NTB</p>
+              <img src={alert.src} alt="alert" />
+              <div className="flex justify-between items-center mt-3">
+                <label className="text-[18px]" htmlFor="perihal">
+                  Perihal
+                </label>
+                <input type={"text"} id="perihal" className="w-[300px] rounded-sm px-2 outline-none h-[40px] border" />
+              </div>
+              <div className="flex justify-between items-center mt-3">
+                <label className="text-[18px]" htmlFor="status">
+                  Status
+                </label>
+                <select id="status" className="w-[300px] rounded-sm px-2 outline-none h-[40px] border">
+                  <option>Proses</option>
+                  <option>Menunggu</option>
+                  <option>Belum Input</option>
+                </select>
+              </div>
+              <div className="flex justify-between mt-3">
+                <label className="text-[18px]" htmlFor="forum">
+                  Detail Forum
+                </label>
+                <textarea type={"text"} id="forum" className="w-[300px] rounded-sm px-2 outline-none h-[140px] border" />
+              </div>
+              <div className="flex justify-end">
+                <div onClick={() => handlePopUp("forum")} className="py-2 px-4 bg-[#FF5001] text-white font-medium mt-3 rounded-sm cursor-pointer">
+                  Broadcast Ke Relawan
+                </div>
+              </div>
+            </div>
+          </>
+        )}
+        {popupPage === "chat" && (
+          <>
+            <div className="absolute inset-y-0 scrollbar scrollbar-thumb-[#9CA3AF] scrollbar-track-[#E5E7EB]  overflow-scroll  right-0 bg-white text-[#374151]">
+              <div
+                onClick={() => {
+                  setPopup(false);
+                  setPopupPage();
+                }}
+                className="absolute pl-2 text-[21px] text-[#9CA3AF] cursor-pointer font-medium"
+              >
+                X
+              </div>
+              <div className="px-[50px] py-[30px]">
+                <p className="text-[21px] font-bold">Logistik Kota Mataram</p>
+                <div className="flex gap-4 mt-2">
+                  <p className="w-[140px] text-[#6B7280]">Tgl Pengajuan</p>
+                  <p>11/12/23</p>
+                </div>
+                <div className="flex gap-4 mt-2">
+                  <p className="w-[140px] text-[#6B7280]">Kebutuhan</p>
+                  <p>Flayer</p>
+                </div>
+                <div className="flex gap-4 mt-2">
+                  <p className="w-[140px] text-[#6B7280]">Status</p>
+                  <p>Belum Input </p>
+                </div>
+                <div className="flex gap-4 mt-2">
+                  <p className="w-[140px] text-[#6B7280]">Detail Pengajuan</p>
+                  <p className="w-[360px]">Ornare magna ultricies sed dapibus pharetra imperdiet nascetur viverra vitae.</p>
+                </div>
+                <div className="flex gap-4 mt-2">
+                  <p className="w-[140px] text-[#6B7280]">Ubah Status</p>
+                  <select className="border py-2 px-6 rounded-sm outline-0 cursor-pointer">
+                    <option>Proses</option>
+                    <option>Menunggu</option>
+                    <option>Belum Input</option>
+                  </select>
+                </div>
+              </div>
+              <div className="bg-[#F3F4F6] p-[20px] min-h-screen pr-8">
+                {/* chat */}
+                <div className=" overflow-y-auto">
+                  <div className="flex justify-center mb-12">
+                    <div className="bg-[#E5E7EB] text-[#6B7280] rounded-full px-2 py-1">12 Desember 2022</div>
+                  </div>
 
-            <div className="fixed bottom-0 z-50 py-3 px-6 w-full flex  bg-[#E5E7EB] items-center">
-              <ChatInput onSendMessage={alert} />
+                  <div className="flex">
+                    <p className=" font-medium bg-white rounded-t-xl rounded-br-xl shadow-lg my-3 p-2 w-[420px] break-words">
+                      Ornare magna ultricies sed dapibus pharetra imperdiet nascetur viverra vitae.asddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd
+                      <p className="text-end text-[12px] font-normal">10.23</p>
+                    </p>
+                  </div>
+                  <div className="flex justify-end ">
+                    <p className="font-medium bg-[#FF5001] text-white  rounded-t-xl rounded-bl-xl shadow-lg my-3 p-2 w-[420px]">
+                      Ornare magna ultricies sed dapibus pharetra imperdiet nascetur viverra vitae.
+                      <p className="text-[12px] font-normal flex gap-1 justify-end items-center">10.23 {<ReadIcon />}</p>
+                    </p>
+                  </div>
+                </div>
+
+                <div className="fixed bottom-0 z-50 py-3 px-6 w-full flex  bg-[#E5E7EB] items-center">
+                  <ChatInput onSendMessage={alert} />
+                </div>
+                {/*  */}
+              </div>
             </div>
-            {/*  */}
-          </div>
-        </div>
+          </>
+        )}
       </div>
 
       {/*  */}
@@ -91,7 +188,7 @@ const Logistik = () => {
               <option>Kecamatan</option>
             </select>
           </div>
-          <div className="flex items-center gap-2 py-2 px-6 rounded-sm cursor-pointer font-medium bg-[#B91C1C] text-white stroke-white">
+          <div onClick={() => handlePopUp("broadcast")} className="flex items-center gap-2 py-2 px-6 rounded-sm cursor-pointer font-medium bg-[#B91C1C] text-white stroke-white">
             <RelawanIcon />
             <p>Broadcast</p>
           </div>
@@ -131,7 +228,7 @@ const Logistik = () => {
                 </td>
                 <td className="w-[88px] px-2 py-3 border-l-2 border-white flex items-center justify-center ">
                   <div className="flex  justify-center gap-3">
-                    <div onClick={() => setPopup(true)} className="cursor-pointer">
+                    <div onClick={() => handlePopUp("chat")} className="cursor-pointer">
                       <ChatIcon />
                     </div>
 
