288e1c19546354194fce723601d05c812bb8a084
wiligetsurvey <wili@getsurvey.id>
2023-02-27 19:49:16 +0800

    publikasi ubah ke link youtube

--- pages/publikasi/TambahData.js
+++ pages/publikasi/TambahData.js
@@ -8,9 +8,12 @@ import PopupBerhasil from "../../src/component/publikasi/PopupBerhasil";
 import axiosFetch from "../../src/API/axiosFetch";
 import Router from "next/router";
 import { useSelector } from "react-redux";
-const DynamicHeader = dynamic(() => import("../../src/component/program/TextEditor"), {
-  ssr: false,
-});
+const DynamicHeader = dynamic(
+  () => import("../../src/component/program/TextEditor"),
+  {
+    ssr: false,
+  }
+);
 
 const TambahData = () => {
   const publikasiStyle = {
@@ -56,6 +59,8 @@ const TambahData = () => {
     },
   ];
 
+  const [videoPreview, setVideoPreview] = useState();
+
   const [selectCategory, setSelecCategory] = useState();
   const [popUp, setPopUp] = useState(false);
   const [berhasil, setBerhasil] = useState(false);
@@ -67,12 +72,19 @@ const TambahData = () => {
     description: "",
     category: "",
     image: "",
+    video: null,
     publication: true,
     kabupaten: "test",
   });
   //   text editor variabel
   const callTextEditor = (setFormProgram, value, formProgram) => {
-    return <DynamicHeader value={value} setFormProgram={setFormProgram} formProgram={formProgram} />;
+    return (
+      <DynamicHeader
+        value={value}
+        setFormProgram={setFormProgram}
+        formProgram={formProgram}
+      />
+    );
   };
 
   const postArtikel = async () => {
@@ -81,9 +93,11 @@ const TambahData = () => {
     a.append("category", formProgram.category);
     if (switchPage === "video") {
       a.append("description", "video");
-      ["mp4", "mkv"].includes(formProgram?.image?.name?.split(".").pop().toLowerCase()) && a.append("image", formProgram.image);
+      a.append("video", formProgram.video);
     } else if (switchPage === "artikel") {
-      ["png", "jpeg", "jpg"].includes(formProgram?.image?.name?.split(".").pop().toLowerCase()) && a.append("image", formProgram.image);
+      ["png", "jpeg", "jpg"].includes(
+        formProgram?.image?.name?.split(".").pop().toLowerCase()
+      ) && a.append("image", formProgram.image);
       a.append("description", formProgram.description);
     }
     a.append("type", "artikel");
@@ -112,9 +126,11 @@ const TambahData = () => {
     a.append("category", formProgram.category);
     if (switchPage === "video") {
       a.append("description", "video");
-      ["mp4", "mkv"].includes(formProgram?.image?.name?.split(".").pop().toLowerCase()) && a.append("image", formProgram.image);
+      a.append("video", formProgram.video);
     } else if (switchPage === "artikel") {
-      ["png", "jpeg", "jpg"].includes(formProgram?.image?.name?.split(".").pop().toLowerCase()) && a.append("image", formProgram.image);
+      ["png", "jpeg", "jpg"].includes(
+        formProgram?.image?.name?.split(".").pop().toLowerCase()
+      ) && a.append("image", formProgram.image);
       a.append("description", formProgram.description);
     }
     a.append("type", "artikel");
@@ -140,7 +156,13 @@ const TambahData = () => {
   console.log(token);
   return (
     <>
-      <PopupBerhasil popUp={popUp} setPopUp={setPopUp} berhasil={berhasil} setBerhasil={setBerhasil} post={postArtikel} />
+      <PopupBerhasil
+        popUp={popUp}
+        setPopUp={setPopUp}
+        berhasil={berhasil}
+        setBerhasil={setBerhasil}
+        post={postArtikel}
+      />
       <div className="flex pl-[42px] mt-[32px] gap-4 border-b-2">
         <div className="flex justify-between w-full pr-[40px]">
           <Logo />
@@ -150,10 +172,16 @@ const TambahData = () => {
               <NewButton title={"Publikasikan"} style={publikasiStyle} />
             </div>
             {/* tombol simpan draft */}
-            <div onClick={postDraftArtikel} className="flex cursor-pointer py-[7px] px-4 items-center border border-[#374151] rounded-md text-[#374151] text-[18px] font-semibold">
+            <div
+              onClick={postDraftArtikel}
+              className="flex cursor-pointer py-[7px] px-4 items-center border border-[#374151] rounded-md text-[#374151] text-[18px] font-semibold"
+            >
               Simpan Draft
             </div>
-            <div onClick={() => Router.back()} className="flex border cursor-pointer border-[#B91C1C] text-[#B91C1C] text-[18px] font-semibold px-4 rounded-md  h-[42px] justify-center items-center">
+            <div
+              onClick={() => Router.back()}
+              className="flex border cursor-pointer border-[#B91C1C] text-[#B91C1C] text-[18px] font-semibold px-4 rounded-md  h-[42px] justify-center items-center"
+            >
               Batal
             </div>
           </div>
@@ -165,11 +193,16 @@ const TambahData = () => {
           <div className="basis-8/12 bg-[#F9FAFB] pl-[42px] pt-[44px] pr-[60px]">
             <div className="flex flex-col gap-2">
               {/* Judul  */}
-              <label id="title" className="text-[#6B7280] text-[16px] font-serif">
+              <label
+                id="title"
+                className="text-[#6B7280] text-[16px] font-serif"
+              >
                 Judul Program
               </label>
               <input
-                onChange={(e) => setFormProgram({ ...formProgram, title: e.target.value })}
+                onChange={(e) =>
+                  setFormProgram({ ...formProgram, title: e.target.value })
+                }
                 className="border border-[#D1D5DB] h-[48px] outline-0 rounded-md p-[12px] text-[#374151] font-medium"
                 value={formProgram.title}
                 type={"text"}
@@ -177,59 +210,80 @@ const TambahData = () => {
               />
 
               {/* description  */}
-              <label id="title" className="text-[] text-[16px] font-serif pt-[60px]">
+              <label
+                id="title"
+                className="text-[] text-[16px] font-serif pt-[60px]"
+              >
                 detail Program
               </label>
               <div className="flex gap-3 text-[#6B7280] text-[16px]">
-                <p onClick={() => setSwitchPage("artikel")} className={`p-2 ${switchPage === "artikel" && "text-white bg-[#E44700] rounded-md font-medium"} cursor-pointer`}>
+                <p
+                  onClick={() => setSwitchPage("artikel")}
+                  className={`p-2 ${
+                    switchPage === "artikel" &&
+                    "text-white bg-[#E44700] rounded-md font-medium"
+                  } cursor-pointer`}
+                >
                   Publikasi Artikel
                 </p>
-                <p onClick={() => setSwitchPage("video")} className={`p-2 ${switchPage === "video" && "text-[white] font-medium bg-[#E44700] rounded-md"} cursor-pointer`}>
+                <p
+                  onClick={() => setSwitchPage("video")}
+                  className={`p-2 ${
+                    switchPage === "video" &&
+                    "text-[white] font-medium bg-[#E44700] rounded-md"
+                  } cursor-pointer`}
+                >
                   Publikasi Video
                 </p>
               </div>
               {switchPage === "artikel" ? (
-                <div className="w-full">{callTextEditor(setFormProgram, formProgram.description, formProgram)}</div>
+                <div className="w-full">
+                  {callTextEditor(
+                    setFormProgram,
+                    formProgram.description,
+                    formProgram
+                  )}
+                </div>
               ) : (
                 <div>
-                  <label htmlFor="video_desctiprion" className=" border border-[#D1D5DB] cursor-pointer">
-                    <div className="shadow-xl border bg-black rounded-md">
-                      {["mkv", "mp4"].includes(formProgram?.image?.name?.split(".").pop().toLowerCase()) ? (
-                        <video className="my-[20px]" controls>
-                          <source src={URL.createObjectURL(formProgram?.image)} type="video/mp4" />
-                          Your browser does not support the video tag.
-                        </video>
-                      ) : (
-                        <p className="text-[18px] text-red-500 font-bold bg-slate-200 px-4 py-12 flex justify-center">{switchPage === "artikel" ? <>Type File Harus "JPG, JPEG, PNG" </> : <>Type File Harus "MP4, MKV" </>}</p>
-                      )}
-                    </div>
-                    <div className="flex flex-col items-center pt-4">
-                      <img src={uploadFile.src} alt="upload here" />
-                      <p className="text-[12px] text-[#000000] font-semibold">
-                        <span className="text-[#FF5001]">Upload a file </span>of drag and drop{" "}
-                      </p>
-                      <p className="text-[12px] font-normal">{switchPage === "artikel" ? <>PNG, JPG, JPEG upto 5MB</> : <>MP4, MKV upto 50MB</>}</p>
-                    </div>
-                    <input
-                      onChange={(e) => {
-                        console.log(e.target.files);
-                        setFormProgram({
-                          ...formProgram,
-                          image: e.target.files[0],
-                        });
-                      }}
-                      id="video_desctiprion"
-                      type="file"
-                      className="hidden"
-                    />
-                  </label>
+                  <span className="mr-1">Link Video</span>
+                  <input
+                    className="border border-[#D1D5DB] p-2 w-[400px]"
+                    type="text"
+                    onChange={(e) => {
+                      setFormProgram({
+                        ...formProgram,
+                        image: null,
+                      });
+                      // setImageFile(null);
+                      setFormProgram({
+                        ...formProgram,
+                        video: e.target.value,
+                      });
+                      setVideoPreview(e.target.value);
+                    }}
+                  />
+                  <br />
+                  <br />
+                  {switchPage === "video" && formProgram.video !== null && (
+                    <iframe
+                      width="420"
+                      height="315"
+                      src={
+                        "https://www.youtube.com/embed/" +
+                        videoPreview?.split("=").pop()
+                      }
+                    ></iframe>
+                  )}
                 </div>
               )}
             </div>
           </div>
           <div className="basis-4/12  pt-[34px] pl-[50px] pr-[41px] border-l-2">
             <div className="flex flex-col gap-2">
-              <p className="text-[18px] text-[#374151] font-bold ">Kategory Program</p>
+              <p className="text-[18px] text-[#374151] font-bold ">
+                Kategory Program
+              </p>
               {categoryProgram.map((res, i) => {
                 return (
                   <div
@@ -241,26 +295,67 @@ const TambahData = () => {
                     value={selectCategory}
                     className="flex items-center gap-4"
                   >
-                    <div className={`h-[30px] w-[30px] rounded-full cursor-pointer ${res.name === selectCategory ? "bg-[#FF5001]" : "border-2 border-[#D1D5DB]"}  `} />
-                    <p className={`text-[16px] font-medium ${res.name === selectCategory ? "text-[#FF5001]" : "text-[#374151]"}`}>{res.name}</p>
+                    <div
+                      className={`h-[30px] w-[30px] rounded-full cursor-pointer ${
+                        res.name === selectCategory
+                          ? "bg-[#FF5001]"
+                          : "border-2 border-[#D1D5DB]"
+                      }  `}
+                    />
+                    <p
+                      className={`text-[16px] font-medium ${
+                        res.name === selectCategory
+                          ? "text-[#FF5001]"
+                          : "text-[#374151]"
+                      }`}
+                    >
+                      {res.name}
+                    </p>
                   </div>
                 );
               })}
-              <div className={`${switchPage === "artikel" ? "visible" : "hidden"}`}>
-                <p className="text-[18px] text-[#374151] font-bold pt-[30px]">{switchPage === "artikel" ? "Thumbnail Artikel" : "Thumbnail Video"}</p>
-                <label htmlFor="file_upload" className={`h-[112px] border border-[#D1D5DB] cursor-pointer`}>
-                  <div className={`${formProgram?.image?.name === undefined ? "hidden" : "visible"}`}>
-                    {["png", "jpeg", "jpg"].includes(formProgram?.image?.name?.split(".").pop().toLowerCase()) ? (
-                      <img src={URL.createObjectURL(formProgram?.image)} alt="preview" />
+              <div
+                className={`${switchPage === "artikel" ? "visible" : "hidden"}`}
+              >
+                <p className="text-[18px] text-[#374151] font-bold pt-[30px]">
+                  {switchPage === "artikel"
+                    ? "Thumbnail Artikel"
+                    : "Thumbnail Video"}
+                </p>
+                <label
+                  htmlFor="file_upload"
+                  className={`h-[112px] border border-[#D1D5DB] cursor-pointer`}
+                >
+                  <div
+                    className={`${
+                      formProgram?.image?.name === undefined
+                        ? "hidden"
+                        : "visible"
+                    }`}
+                  >
+                    {["png", "jpeg", "jpg"].includes(
+                      formProgram?.image?.name?.split(".").pop().toLowerCase()
+                    ) ? (
+                      <img
+                        src={URL.createObjectURL(formProgram?.image)}
+                        alt="preview"
+                      />
                     ) : (
-                      <p className="text-[18px] text-red-500 font-bold bg-slate-200 px-4 py-12 flex justify-center">{switchPage === "artikel" ? <>Type File Harus "JPG, JPEG, PNG" </> : <>Type File Harus "MP4, MKV" </>}</p>
+                      <p className="text-[18px] text-red-500 font-bold bg-slate-200 px-4 py-12 flex justify-center">
+                        {switchPage === "artikel" ? (
+                          <>Type File Harus "JPG, JPEG, PNG" </>
+                        ) : (
+                          <>Type File Harus "MP4, MKV" </>
+                        )}
+                      </p>
                     )}
                   </div>
 
                   <div className="flex flex-col items-center pt-4">
                     <img src={uploadFile.src} alt="upload here" />
                     <p className="text-[12px] text-[#000000] font-semibold">
-                      <span className="text-[#FF5001]">Upload a file </span>of drag and drop{" "}
+                      <span className="text-[#FF5001]">Upload a file </span>of
+                      drag and drop{" "}
                     </p>
                     <p className="text-[12px] font-normal">
                       <>PNG, JPG, JPEG upto 5MB</>{" "}
--- src/component/homepage/Publikasi.js
+++ src/component/homepage/Publikasi.js
@@ -1,23 +1,50 @@
 import React from "react";
 
 const Publikasi = ({ data }) => {
-  const monthNames = ["Januari", "Februari", "Maret", "April", "Mei", "Juni", "Juli", "Agustus", "September", "Oktober", "Nopember", "Desember"];
-  // console.log(data);
+  const monthNames = [
+    "Januari",
+    "Februari",
+    "Maret",
+    "April",
+    "Mei",
+    "Juni",
+    "Juli",
+    "Agustus",
+    "September",
+    "Oktober",
+    "Nopember",
+    "Desember",
+  ];
+  console.log("xxx");
+  console.log(data);
+  console.log("xxx");
   return (
     <div className="font-bold text-[32px] text-slate-700">
       {/* {data?.image && <img className="w-[540px] h-[420px] rounded-xl object-cover" src={process.env.NEXT_PUBLIC_BASE_URL_IMAGE + data?.image} alt="Frame-2519" border="0"></img>} */}
       <div className="flex mt-8">
-        {data?.image && (
-          <video className="w-[828.27px] h-[552.18px] rounded-xl object-fill" controls>
-            <source src={process.env.NEXT_PUBLIC_BASE_URL_IMAGE + data?.image} type="video/mp4" />
-            Your browser does not support the video tag.
-          </video>
+        {data.length !== 0 && (
+          <>asdasd</>
+          // <iframe
+          //   width="420"
+          //   height="315"
+          //   src={"https://www.youtube.com/embed/" + data.video.split("=").pop()}
+          // ></iframe>
         )}
       </div>
       <div className="flex items-center mt-3 gap-6">
-        <p className="text-[#FF5001] text-[21px] font-medium">{data?.category}</p>
+        <p className="text-[#FF5001] text-[21px] font-medium">
+          {data?.category}
+        </p>
         <p className="text-[21px] font-medium">
-          {data?.createdAt?.split("T").shift().split("-")[2]} {monthNames[new Date(data?.createdAt?.split("T").shift().split("-")[1]).getMonth()]} {data?.createdAt?.split("T").shift().split("-")[0]}
+          {data?.createdAt?.split("T").shift().split("-")[2]}{" "}
+          {
+            monthNames[
+              new Date(
+                data?.createdAt?.split("T").shift().split("-")[1]
+              ).getMonth()
+            ]
+          }{" "}
+          {data?.createdAt?.split("T").shift().split("-")[0]}
         </p>
       </div>
       <p className="text-[26px]">{data?.title}</p>
