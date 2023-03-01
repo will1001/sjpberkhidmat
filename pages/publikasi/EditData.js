2f27443ae48f560a21e6b938bab16f6d601b05a9
wiligetsurvey <wili@getsurvey.id>
2023-03-01 22:12:40 +0800

    perbaiki edit data publikasi

--- pages/publikasi/EditData.js
+++ pages/publikasi/EditData.js
@@ -8,9 +8,12 @@ import PopupBerhasil from "../../src/component/publikasi/PopupBerhasil";
 import axiosFetch from "../../src/API/axiosFetch";
 import Router from "next/router";
 import { useSelector } from "react-redux";
-const DynamicHeader = dynamic(() => import("../../src/component/program/TextEditor"), {
-  ssr: false,
-});
+const DynamicHeader = dynamic(
+  () => import("../../src/component/program/TextEditor"),
+  {
+    ssr: false,
+  }
+);
 
 const EditData = () => {
   const publikasiStyle = {
@@ -76,10 +79,20 @@ const EditData = () => {
   });
   //   text editor variabel
   const callTextEditor = (setFormProgram, value, formProgram) => {
-    return <DynamicHeader value={value} setFormProgram={setFormProgram} formProgram={formProgram} />;
+    return (
+      <DynamicHeader
+        value={value}
+        setFormProgram={setFormProgram}
+        formProgram={formProgram}
+      />
+    );
   };
 
   const postArtikel = async () => {
+    console.log(switchPage);
+    console.log(formProgram);
+    console.log(formProgram.title);
+
     const a = new FormData();
     a.append("title", formProgram.title);
     a.append("category", formProgram.category);
@@ -88,7 +101,9 @@ const EditData = () => {
       a.append("video", formProgram.video);
     } else if (switchPage === "artikel") {
       if (changeImage !== undefined) {
-        ["png", "jpeg", "jpg"].includes(changeImage?.name?.split(".").pop().toLowerCase()) && a.append("image", changeImage);
+        ["png", "jpeg", "jpg"].includes(
+          changeImage?.name?.split(".").pop().toLowerCase()
+        ) && a.append("image", changeImage);
       }
       a.append("description", formProgram.description);
     }
@@ -117,10 +132,13 @@ const EditData = () => {
     a.append("category", formProgram.category);
     if (switchPage === "video") {
       a.append("description", "video");
+      a.append("video", "video");
       videoPreview !== undefined && a.append("video", videoPreview);
     } else if (switchPage === "artikel") {
       if (changeImage !== undefined) {
-        ["png", "jpeg", "jpg"].includes(changeImage?.name?.split(".").pop().toLowerCase()) && a.append("image", changeImage);
+        ["png", "jpeg", "jpg"].includes(
+          changeImage?.name?.split(".").pop().toLowerCase()
+        ) && a.append("image", changeImage);
       }
       a.append("description", formProgram.description);
     }
@@ -152,6 +170,7 @@ const EditData = () => {
         title: Router.query.title,
         image: Router.query.file,
         category: Router.query.category,
+        video: Router.query.video,
       });
     } else {
       setFormProgram({
@@ -168,7 +187,13 @@ const EditData = () => {
 
   return (
     <>
-      <PopupBerhasil popUp={popUp} setPopUp={setPopUp} berhasil={berhasil} setBerhasil={setBerhasil} post={postArtikel} />
+      <PopupBerhasil
+        popUp={popUp}
+        setPopUp={setPopUp}
+        berhasil={berhasil}
+        setBerhasil={setBerhasil}
+        post={postArtikel}
+      />
       <div className="flex pl-[42px] mt-[32px] gap-4 border-b-2">
         <div className="flex justify-between w-full pr-[40px]">
           <Logo />
@@ -178,10 +203,16 @@ const EditData = () => {
               <NewButton title={"Publikasikan"} style={publikasiStyle} />
             </div>
             {/* tombol simpan draft */}
-            <div onClick={postDraftArtikel} className="flex cursor-pointer py-[7px] px-4 items-center border border-[#374151] rounded-md text-[#374151] text-[18px] font-semibold">
+            <div
+              onClick={postDraftArtikel}
+              className="flex cursor-pointer py-[7px] px-4 items-center border border-[#374151] rounded-md text-[#374151] text-[18px] font-semibold"
+            >
               Simpan Draft
             </div>
-            <div onClick={() => Router.back()} className="flex border cursor-pointer border-[#B91C1C] text-[#B91C1C] text-[18px] font-semibold px-4 rounded-md  h-[42px] justify-center items-center">
+            <div
+              onClick={() => Router.back()}
+              className="flex border cursor-pointer border-[#B91C1C] text-[#B91C1C] text-[18px] font-semibold px-4 rounded-md  h-[42px] justify-center items-center"
+            >
               Batal
             </div>
           </div>
@@ -196,7 +227,9 @@ const EditData = () => {
             </label>
 
             <input
-              onChange={(e) => setFormProgram({ ...formProgram, title: e.target.value })}
+              onChange={(e) =>
+                setFormProgram({ ...formProgram, title: e.target.value })
+              }
               className="border border-[#D1D5DB] h-[48px] outline-0 rounded-md p-[12px] text-[#374151] font-medium"
               value={formProgram?.title}
               type={"text"}
@@ -204,19 +237,40 @@ const EditData = () => {
             />
 
             {/* description  */}
-            <label id="title" className="text-[] text-[16px] font-serif pt-[60px]">
+            <label
+              id="title"
+              className="text-[] text-[16px] font-serif pt-[60px]"
+            >
               detail Program
             </label>
             <div className="flex gap-3 text-[#6B7280] text-[16px]">
-              <p onClick={() => setSwitchPage("artikel")} className={`p-2 ${switchPage === "artikel" && "text-white bg-[#E44700] rounded-md font-medium"} cursor-pointer`}>
+              <p
+                onClick={() => setSwitchPage("artikel")}
+                className={`p-2 ${
+                  switchPage === "artikel" &&
+                  "text-white bg-[#E44700] rounded-md font-medium"
+                } cursor-pointer`}
+              >
                 Publikasi Artikel
               </p>
-              <p onClick={() => setSwitchPage("video")} className={`p-2 ${switchPage === "video" && "text-[white] font-medium bg-[#E44700] rounded-md"} cursor-pointer`}>
+              <p
+                onClick={() => setSwitchPage("video")}
+                className={`p-2 ${
+                  switchPage === "video" &&
+                  "text-[white] font-medium bg-[#E44700] rounded-md"
+                } cursor-pointer`}
+              >
                 Publikasi Video
               </p>
             </div>
             {switchPage === "artikel" ? (
-              <div className="w-full">{callTextEditor(setFormProgram, formProgram.description, formProgram)}</div>
+              <div className="w-full">
+                {callTextEditor(
+                  setFormProgram,
+                  formProgram.description,
+                  formProgram
+                )}
+              </div>
             ) : (
               <div>
                 <span className="mr-1">Link Video</span>
@@ -235,13 +289,38 @@ const EditData = () => {
                     });
                     setVideoPreview(e.target.value);
                   }}
+                  value={formProgram?.video}
                 />
                 <br />
                 <br />
                 {videoPreview !== undefined ? (
-                  <> {switchPage === "video" && formProgram.video !== null && <iframe width="420" height="315" src={"https://www.youtube.com/embed/" + videoPreview?.split("=").pop()}></iframe>}</>
+                  <>
+                    {" "}
+                    {switchPage === "video" && formProgram.video !== null && (
+                      <iframe
+                        width="420"
+                        height="315"
+                        src={
+                          "https://www.youtube.com/embed/" +
+                          videoPreview?.split("=").pop()
+                        }
+                      ></iframe>
+                    )}
+                  </>
                 ) : (
-                  <> {switchPage === "video" && Router.query.video && <iframe width="420" height="315" src={"https://www.youtube.com/embed/" + Router.query.video?.split("=").pop()}></iframe>}</>
+                  <>
+                    {" "}
+                    {switchPage === "video" && Router.query.video && (
+                      <iframe
+                        width="420"
+                        height="315"
+                        src={
+                          "https://www.youtube.com/embed/" +
+                          Router.query.video?.split("=").pop()
+                        }
+                      ></iframe>
+                    )}
+                  </>
                 )}
               </div>
             )}
@@ -249,7 +328,9 @@ const EditData = () => {
         </div>
         <div className="basis-4/12  pt-[34px] pl-[50px] pr-[41px] border-l-2">
           <div className="flex flex-col gap-2">
-            <p className="text-[18px] text-[#374151] font-bold ">Kategory Program</p>
+            <p className="text-[18px] text-[#374151] font-bold ">
+              Kategory Program
+            </p>
             {categoryProgram.map((res, i) => {
               return (
                 <div
@@ -261,30 +342,70 @@ const EditData = () => {
                   value={selectCategory}
                   className="flex items-center gap-4"
                 >
-                  <div className={`h-[30px] w-[30px] rounded-full cursor-pointer ${res.name === formProgram?.category ? "bg-[#FF5001]" : "border-2 border-[#D1D5DB]"}  `} />
-                  <p className={`text-[16px] font-medium ${res.name === formProgram.category ? "text-[#FF5001]" : "text-[#374151]"}`}>{res.name}</p>
+                  <div
+                    className={`h-[30px] w-[30px] rounded-full cursor-pointer ${
+                      res.name === formProgram?.category
+                        ? "bg-[#FF5001]"
+                        : "border-2 border-[#D1D5DB]"
+                    }  `}
+                  />
+                  <p
+                    className={`text-[16px] font-medium ${
+                      res.name === formProgram.category
+                        ? "text-[#FF5001]"
+                        : "text-[#374151]"
+                    }`}
+                  >
+                    {res.name}
+                  </p>
                 </div>
               );
             })}
 
-            <div className={`${switchPage === "artikel" ? "visible" : "hidden"}`}>
-              <p className="text-[18px] text-[#374151] font-bold pt-[30px]">{switchPage === "artikel" ? "Thumbnail Artikel" : "Thumbnail Video"}</p>
-              <label htmlFor="file_upload" className={`h-[112px] border border-[#D1D5DB] cursor-pointer`}>
+            <div
+              className={`${switchPage === "artikel" ? "visible" : "hidden"}`}
+            >
+              <p className="text-[18px] text-[#374151] font-bold pt-[30px]">
+                {switchPage === "artikel"
+                  ? "Thumbnail Artikel"
+                  : "Thumbnail Video"}
+              </p>
+              <label
+                htmlFor="file_upload"
+                className={`h-[112px] border border-[#D1D5DB] cursor-pointer`}
+              >
                 {changeImage !== undefined ? (
                   <>
                     {" "}
                     <div>
-                      {["png", "jpeg", "jpg"].includes(changeImage?.name?.split(".").pop().toLowerCase()) ? (
-                        <img src={URL.createObjectURL(changeImage)} alt="preview" />
+                      {["png", "jpeg", "jpg"].includes(
+                        changeImage?.name?.split(".").pop().toLowerCase()
+                      ) ? (
+                        <img
+                          src={URL.createObjectURL(changeImage)}
+                          alt="preview"
+                        />
                       ) : (
-                        <p className="text-[18px] text-red-500 font-bold bg-slate-200 px-4 py-12 flex justify-center">{switchPage === "artikel" ? <>Type File Harus "JPG, JPEG, PNG" </> : <>Type File Harus "MP4, MKV" </>}</p>
+                        <p className="text-[18px] text-red-500 font-bold bg-slate-200 px-4 py-12 flex justify-center">
+                          {switchPage === "artikel" ? (
+                            <>Type File Harus "JPG, JPEG, PNG" </>
+                          ) : (
+                            <>Type File Harus "MP4, MKV" </>
+                          )}
+                        </p>
                       )}
                     </div>
                   </>
                 ) : (
                   <>
                     <div>
-                      <img src={process.env.NEXT_PUBLIC_BASE_URL_IMAGE + Router.query.file} alt="preview" />
+                      <img
+                        src={
+                          process.env.NEXT_PUBLIC_BASE_URL_IMAGE +
+                          Router.query.file
+                        }
+                        alt="preview"
+                      />
                     </div>
                   </>
                 )}
@@ -292,7 +413,8 @@ const EditData = () => {
                 <div className="flex flex-col items-center pt-4">
                   <img src={uploadFile.src} alt="upload here" />
                   <p className="text-[12px] text-[#000000] font-semibold">
-                    <span className="text-[#FF5001]">Upload a file </span>of drag and drop{" "}
+                    <span className="text-[#FF5001]">Upload a file </span>of
+                    drag and drop{" "}
                   </p>
                   <p className="text-[12px] font-normal">
                     <>PNG, JPG, JPEG upto 5MB</>{" "}
