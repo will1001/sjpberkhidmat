713a1764732ac066825fcbc17dc54c8f313a91ed
wiligetsurvey <wili@getsurvey.id>
2023-03-28 15:37:49 +0800

    fix : bug PWA

--- next.config.js
+++ next.config.js
@@ -1,21 +1,22 @@
 /** @type {import('next').NextConfig} */
 
-const withPWA = require('next-pwa')({
-  dest: 'public',
-})
+const withPWA = require("next-pwa")({
+  dest: "public",
+});
 
 const nextConfig = {
   reactStrictMode: true,
   images: {
     unoptimized: true,
   },
-  // pwa: {
-  //   dest: "public",
-  //   disable: process.env.NODE_ENV === "development",
-  //   register: true,
-  //   scope: "/",
-  //   sw: "service-worker.js",
-  // },
+  pwa: {
+    dest: "public",
+    disable: process.env.NODE_ENV === "development",
+    register: true,
+    scope: "/",
+    skipWaiting: true,
+    // sw: "service-worker.js",
+  },
 };
 
 module.exports = withPWA(nextConfig);
