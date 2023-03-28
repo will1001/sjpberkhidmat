3429d008961c493851c068ef700bc9a009af0199
wiligetsurvey <wili@getsurvey.id>
2023-03-28 14:28:35 +0800

    setting PWA

--- next.config.js
+++ next.config.js
@@ -1,9 +1,21 @@
 /** @type {import('next').NextConfig} */
+
+const withPWA = require('next-pwa')({
+  dest: 'public',
+})
+
 const nextConfig = {
   reactStrictMode: true,
   images: {
     unoptimized: true,
   },
+  pwa: {
+    dest: "public",
+    disable: process.env.NODE_ENV === "development",
+    register: true,
+    scope: "/",
+    sw: "service-worker.js",
+  },
 };
 
-module.exports = nextConfig;
+module.exports = withPWA(nextConfig);
