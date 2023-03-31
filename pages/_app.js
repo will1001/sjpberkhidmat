7934c2d5c2e6c38b888a54bad6cfb81615092c62
wiligetsurvey <wili@getsurvey.id>
2023-04-01 07:29:59 +0800

    feat : save token notif

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,15 @@
 import "../styles/globals.css";
-import { Provider } from "react-redux";
+import { Provider, useSelector } from "react-redux";
 import { persistor, store } from "../src/redux/store";
 import { PersistGate } from "redux-persist/integration/react";
 import { useEffect } from "react";
 import { app } from "../src/firebase/firebase";
 import { getToken, getMessaging } from "firebase/messaging";
+import axiosFetch from "../src/API/axiosFetch";
 
 export default function App({ Component, pageProps }) {
+  const token = useSelector((state) => state.user.token);
+
   const requestNotifPermission = async () => {
     const messaging = getMessaging(app);
 
@@ -15,11 +18,17 @@ export default function App({ Component, pageProps }) {
       getToken(messaging, {
         vapidKey:
           "BOZ5lS6LL3TkU3LI1WcH_S_2Vvo_Fg6FiDo7BUmqylHHZDmSMq6sRNcL-ObBCl6jN3geNd0anA7u_pOf8pP6TD8",
-      }).then((token) => {
+      }).then(async (tokenFcm) => {
         if (token) {
           console.log("token");
-          console.log(token);
+          console.log(tokenFcm);
           console.log("token");
+          const a = new FormData();
+          a.append("token", tokenFcm);
+
+          {
+            await axiosFetch("post", `user/token/save`, a, token);
+          }
         } else {
           console.log("no");
         }
