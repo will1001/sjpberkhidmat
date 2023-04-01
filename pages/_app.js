738f2d46df60bd8f94f9334589c54b056c977773
wiligetsurvey <wili@getsurvey.id>
2023-04-01 13:24:02 +0800

    fix : save token fcm

--- pages/_app.js
+++ pages/_app.js
@@ -2,47 +2,12 @@ import "../styles/globals.css";
 import { Provider } from "react-redux";
 import { persistor, store } from "../src/redux/store";
 import { PersistGate } from "redux-persist/integration/react";
-import { app } from "../firebase";
-import { getToken, getMessaging } from "firebase/messaging";
-import { useEffect } from "react";
-// import axiosFetch from "../src/API/axiosFetch";
+
 
 export default function App({ Component, pageProps }) {
-  // const token = useSelector((state) => state.user.token);
+ 
 
-  const requestNotifPermission = async () => {
-    const messaging = getMessaging(app);
-
-    const permission = await Notification.requestPermission();
-    if (permission === "granted") {
-      getToken(messaging, {
-        vapidKey:
-          "BOZ5lS6LL3TkU3LI1WcH_S_2Vvo_Fg6FiDo7BUmqylHHZDmSMq6sRNcL-ObBCl6jN3geNd0anA7u_pOf8pP6TD8",
-      }).then(async (tokenFcm) => {
-        if (tokenFcm) {
-          console.log(tokenFcm);
-          console.log("token");
-          console.log("token");
-          // const a = new FormData();
-          // a.append("token", tokenFcm);
-
-          // {
-          //   await axiosFetch("post", `user/token/save`, a, token);
-          // }
-        } else {
-          console.log("no granted");
-        }
-      });
-    }
-
-    if (permission === "denied") {
-      alert("Anda menolak notifikasi akses");
-    }
-  };
-
-  useEffect(() => {
-    requestNotifPermission();
-  }, []);
+  
   return (
     <>
       <Provider store={store}>
--- pages/index.js
+++ pages/index.js
@@ -2,12 +2,45 @@ import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { setIdPeriode } from "../src/redux/panelReducer";
 import HomePage from "./HomePage";
-
-
+import { app } from "../firebase";
+import { getToken, getMessaging } from "firebase/messaging";
+import axiosFetch from "../src/API/axiosFetch";
 export default function Home() {
   const dispatch = useDispatch();
+  const token = useSelector((state) => state.user.token);
 
+  const requestNotifPermission = async () => {
+    const messaging = getMessaging(app);
+
+    const permission = await Notification.requestPermission();
+    if (permission === "granted") {
+      getToken(messaging, {
+        vapidKey:
+          "BOZ5lS6LL3TkU3LI1WcH_S_2Vvo_Fg6FiDo7BUmqylHHZDmSMq6sRNcL-ObBCl6jN3geNd0anA7u_pOf8pP6TD8",
+      }).then(async (tokenFcm) => {
+        if (tokenFcm) {
+          console.log(tokenFcm);
+          console.log("token");
+          console.log("token");
+          const a = new FormData();
+          a.append("token", tokenFcm);
+
+          {
+            await axiosFetch("post", `user/token/save`, a, token);
+          }
+        } else {
+          console.log("no granted");
+        }
+      });
+    }
+
+    if (permission === "denied") {
+      alert("Anda menolak notifikasi akses");
+    }
+  };
   useEffect(() => {
+    requestNotifPermission();
+
     dispatch(
       setIdPeriode({ idPeriode: "6cea57ca-725e-4bbb-84b5-7ccc88f0cd51" })
     );
